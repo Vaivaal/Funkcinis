c9bf5bcc1a6876b93cd82ef79ae61162a1e60d6c
shilingas <mariussilingas5@gmail.com>
2022-10-29 20:19:39 +0300

    FP 2 task

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,26 +1,134 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use <$>" #-}
 module Lib2(renderDocument, hint, gameStart) where
 
-import Types ( ToDocument(..), Document, Check )
+import Types ( ToDocument(..), Document(..), Check(..), Coord(..) )
 import Lib1 (State(..))
+import Data.List (delete)
 
+instance Eq State where
+    (==) (State a1 b1 c1 d1 e1) (State a2 b2 c2 d2 e2) = a1 == a2 && b1 == b2 && c1 == c2 && d1 == d2 && e1 == e2
 -- IMPLEMENT
 -- First, make Check an instance of ToDocument class
+instance ToDocument Check where
+    toDocument Check {coords = []} = DMap [("coords", DList [])] 
+    toDocument Check {coords = a} = DMap [("coords", DList (toList' a))]
+        where
+            toList' :: [Coord] -> [Document]
+            toList' [] = []
+            toList' (x:xs) = coordToMap x : toList' xs
+                where
+                    coordToMap :: Coord -> Document
+                    coordToMap Coord {col = c, row = r} = DMap [("col", DInteger c),("row", DInteger r)]
 
 -- IMPLEMENT
 -- Renders document to yaml
 renderDocument :: Document -> String
-renderDocument _ = error "Implement me"
+renderDocument DNull = "null"
+renderDocument (DInteger num) = show num
+renderDocument (DString str) = str
+renderDocument (DList list) = renderList list 0 False
+renderDocument (DMap lot) = renderMap lot 0 False
+
+renderList :: [Document] -> Int -> Bool -> String
+renderList [] _ _ = ""
+renderList (DNull:xs) a b = renderTab a b ++ "- " ++ "null" ++ "\n" ++ renderList xs a True
+renderList ((DInteger num):xs) a b = renderTab a b ++ "- " ++ show num ++ "\n" ++ renderList xs a True
+renderList ((DString str):xs) a b = renderTab a b ++ "- " ++ str ++ "\n" ++ renderList xs a True
+renderList ((DList []):xs) a b = renderTab a b ++ "- \n" ++ renderList xs a True
+renderList ((DList list):xs) a b = renderTab a b ++ "- " ++ renderList list (a + 1) False ++ renderList xs a True
+renderList ((DMap lot):xs) a b = renderTab a b ++ "- " ++ renderMap lot (a + 1) b ++ renderList xs a b
+
+renderMap :: [(String, Document)] -> Int -> Bool -> String
+renderMap [] _ _ = ""
+renderMap ((s, DNull):xs) a b = renderTab a b ++ s ++ ": " ++ "null" ++ "\n" ++ renderMap xs a True
+renderMap ((s, DInteger num):xs) a b = renderTab a b ++ s ++ ": " ++ show num ++ "\n" ++ renderMap xs a True
+renderMap ((s, DString str):xs) a b = renderTab a b ++ s ++ ": " ++ str ++ "\n" ++ renderMap xs a True
+renderMap ((s, DList list):xs) a b = renderTab a b ++ s ++ ":\n" ++ renderList list a b ++ renderMap xs a True
+renderMap ((s, DMap lot):xs) a b = renderTab a b ++ s ++ ":\n" ++ renderMap lot (a + 1) True ++ renderMap xs a True
+
+renderTab :: Int -> Bool -> String
+renderTab a b
+    | not b = ""
+    | a == 0 = ""
+    | otherwise = "  " ++ renderTab (a - 1) b
 
 -- IMPLEMENT
 -- This adds game data to initial state
 -- Errors are reported via Either but not error 
 gameStart :: State -> Document -> Either String State
-gameStart (State l) d = Right $ State $ ("Game started: " ++ show d) : l
+gameStart _ doc = do
+    c <- toIntList(findData "occupied_cols" $ toMap doc)
+    r <- toIntList(findData "occupied_rows" $ toMap doc)
+    h <- toInt(findData "number_of_hints" $ toMap doc)
+    return (State [] [] c r h)
+--gameStart _ doc = Left (show doc)
+
+toMap :: Document -> Either String [(String, Document)]
+toMap (DMap x) = Right x
+toMap DNull = Right [("", DNull)]
+toMap e = Left ("invalid " ++ show e ++ " argument, expected DMap")
+
+toInt :: Either String Document -> Either String Int
+toInt (Right (DInteger x)) = Right x
+--toInt (Right DNull) = Left "invalid argument, expected DInteger"
+toInt (Right e) = Left ("invalid " ++ show e ++ " argument, expected DInteger")
+toInt (Left e) = Left e
+
+toIntList :: Either String Document -> Either String [Int] 
+toIntList (Right (DList [])) = Right []
+toIntList (Right (DList (DInteger x:xs))) = 
+    case  toIntList (Right (DList xs)) of
+        Right num -> Right (x : num)
+        Left e -> Left e
+toIntList (Right e) = Left ("invalid " ++ show e ++ " argument, expected DList [DInteger...]")
+toIntList (Left e) = Left e
+    
+
+findData :: String -> Either String [(String, Document)] -> Either String Document
+findData category (Right []) = Left ("could not find " ++ show category)
+findData category (Right gameMap)
+    | category == fst (head gameMap) = Right (snd (head gameMap))
+    | otherwise = findData category (Right (tail gameMap))
+findData _ (Left e) = Left e
 
 -- IMPLEMENT
 -- Adds hint data to the game state
 -- Errors are reported via Either but not error 
+
 hint :: State -> Document -> Either String State
-hint (State l) h = Right $ State $ ("Hint " ++ show h) : l
+hint (State _ toggled cols rows hintsNo) doc = 
+    case toMap doc of
+        Right newMap -> case hintsGenerator (toMap(snd (head newMap))) of
+            Right hints -> Right (noDuplicates (State hints toggled cols rows hintsNo))
+            Left e -> Left e
+        Left err -> Left err
+
+hintsGenerator :: Either String [(String, Document)] -> Either String [Coord]
+hintsGenerator (Right [("", DNull)]) = Right []
+hintsGenerator (Right x) = 
+    case hintGenerator (toMap(snd (head x))) of
+        Right listHead -> case hintsGenerator (toMap (snd (last x))) of
+            Right listTail -> if snd (head x) == DNull then Right [] else Right (listHead : listTail)
+            Left e -> Left e
+        Left e -> Left e
+hintsGenerator (Left e) = Left e
+
+hintGenerator :: Either String [(String, Document)] -> Either String Coord
+hintGenerator (Right x) = 
+    case toInt (Right (snd (head x))) of
+        Right c -> 
+            case toInt (Right (snd (last x))) of 
+                Right r -> Right (Coord {col = c, row = r})
+                Left e -> Left e
+        Left err -> Left err  
+hintGenerator (Left e) = Left e
+
+noDuplicates :: State -> State
+noDuplicates (State hinted toggled cols rows hintsNo) = State hinted (elimination hinted toggled) cols rows hintsNo
+
+elimination :: [Coord] -> [Coord] -> [Coord]
+elimination [] toggled = toggled
+elimination (x:xs) toggled = elimination xs (delete x toggled)
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,8 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Lib2 (renderDocument, gameStart, hint)
-import Types (Document(..))
+import Types (Document(..), Coord(..))
+import Lib1 (State(..))
 
 main :: IO ()
 main = defaultMain (testGroup "Tests" [
@@ -18,20 +19,143 @@ toYamlTests = testGroup "Document to yaml"
         renderDocument (DInteger 5) @?= "5"
     , testCase "list of ints" $
         renderDocument (DList [DInteger 5, DInteger 6]) @?= listOfInts
-    -- IMPLEMENT more test cases:
+    , testCase "String" $
+        renderDocument (DString "String test") @?= "String test"
+    , testCase "list of primitives" $
+        renderDocument (DList [DInteger 5, DInteger 6, DNull, DString "testing string", DInteger 123, DNull, DString "abc", DString "12345"]) @?= listOfPrimitives
+    , testCase "List of lists" $
+        renderDocument (DList [DList[DInteger 1, DInteger 2, DInteger 3], DList[], DList[DString "Hello", DNull, DString "Hi"]]) @?= listOfLists
+    , testCase "list in list in list in list" $
+        renderDocument (DList [DList [DList [DList [DNull]]]]) @?= listInListInListInList
+    , testCase "list in list in list in list with numbers" $
+        renderDocument (DList [DList [DInteger 4, DInteger 5, DInteger 6, DList [DList [DString "Sveikas", DString "Pasauli"]], DInteger 7]]) @?= listInListInListInListWithNumbers
+    , testCase "DMap" $
+        renderDocument (DMap [("coords", DList [DMap [("col", DInteger 1), ("row", DInteger 6)], DMap [("col", DInteger 1), ("row", DInteger 9)]])]) @?= coords2
+    , testCase "DDMap" $
+        renderDocument (DMap [("Test1", DMap [("Test2.1", DMap [("Test3", DNull)]), ("Test2.2", DMap [("Test3", DList[DInteger 7, DInteger 8]), ("Test3", DInteger 4)])])]) @?= dMapTest
+        -- IMPLEMENT more test cases:
     -- * other primitive types/values
     -- * nested types
   ]
 
+dMapTest :: String
+dMapTest = unlines [
+    "Test1:"
+  , "  Test2.1:"
+  , "    Test3: null"
+  , "  Test2.2:"
+  , "    Test3:"
+  , "    - 7"
+  , "    - 8"
+  , "    Test3: 4"
+  ]
+
+coords2 :: String
+coords2 = unlines [
+      "coords:"
+    , "- col: 1"
+    , "  row: 6"
+    , "- col: 1"
+    , "  row: 9"
+  ]
+
+listInListInListInListWithNumbers :: String
+listInListInListInListWithNumbers = unlines [
+      "- - 4"
+    , "  - 5"
+    , "  - 6"
+    , "  - - - Sveikas"
+    , "      - Pasauli"
+    , "  - 7"
+  ]
+
+listInListInListInList :: String
+listInListInListInList = unlines [
+    "- - - - null"
+  ]
+
+listOfLists :: String
+listOfLists = unlines [
+      "- - 1"
+    , "  - 2"
+    , "  - 3"
+    , "- "
+    , "- - Hello"
+    , "  - null"
+    , "  - Hi"
+  ]
+
+listOfPrimitives :: String
+listOfPrimitives = unlines [
+      "- 5"
+    , "- 6"
+    , "- null"
+    , "- testing string"
+    , "- 123"
+    , "- null"
+    , "- abc"
+    , "- 12345"
+  ]
+
 listOfInts :: String
 listOfInts = unlines [
-      "---"
-    , "- 5"
+      "- 5"
     , "- 6"
   ]
 
 gameStartTests :: TestTree
-gameStartTests = testGroup "Test start document" []
+gameStartTests = testGroup "Test start document" 
+  [   testCase "game start from server" $
+        gameStart (State {hintedCells = [], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 0}) serverDoc @?= Right (State {hintedCells = [], toggledCells = [], occupiedCols = [2,0,2,2,2,0,6,0,3,3], occupiedRows = [0,2,4,2,4,1,3,2,1,1], noOfHints = 10})
+    , testCase "incorrect category string" $
+        gameStart (State {hintedCells = [], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 0}) serverDoc2 @?= Left "could not find \"occupied_cols\""
+    , testCase "DMap instead of DList" $
+        gameStart (State {hintedCells = [], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 0}) serverDoc3 @?= Left "invalid DMap [(\"kazkas\",DInteger 5)] argument, expected DList [DInteger...]"
+    , testCase "DString in DList instead of DInteger in DList" $
+        gameStart (State {hintedCells = [], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 0}) serverDoc4 @?= Left "invalid DList [DString \"labas\"] argument, expected DList [DInteger...]"
+    , testCase "DNull instead of DInteger" $
+        gameStart (State {hintedCells = [], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 0}) serverDoc5 @?= Left "invalid DNull argument, expected DInteger"
+    , testCase "DInteger instead of DMap" $
+        gameStart (State {hintedCells = [], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 0}) (DInteger 5) @?= Left "invalid DInteger 5 argument, expected DMap"
+    
+  ]
+
+serverDoc :: Document
+serverDoc = DMap [("number_of_hints",DInteger 10),("occupied_cols",DList [DInteger 2,DInteger 0,DInteger 2,DInteger 2,DInteger 2,DInteger 0,DInteger 6,DInteger 0,DInteger 3,DInteger 3]),("occupied_rows",DList [DInteger 0,DInteger 2,DInteger 4,DInteger 2,DInteger 4,DInteger 1,DInteger 3,DInteger 2,DInteger 1,DInteger 1])]
+
+serverDoc2 :: Document
+serverDoc2 = DMap [("number_of_hints",DInteger 10),("occupied_colss",DList [DInteger 2,DInteger 0,DInteger 2,DInteger 2,DInteger 2,DInteger 0,DInteger 6,DInteger 0,DInteger 3,DInteger 3]),("occupied_rows",DList [DInteger 0,DInteger 2,DInteger 4,DInteger 2,DInteger 4,DInteger 1,DInteger 3,DInteger 2,DInteger 1,DInteger 1])]
+
+serverDoc3 :: Document
+serverDoc3 = DMap [("number_of_hints",DInteger 10),("occupied_cols",DMap [("kazkas", DInteger 5)]),("occupied_rows",DList [DInteger 0,DInteger 2,DInteger 4,DInteger 2,DInteger 4,DInteger 1,DInteger 3,DInteger 2,DInteger 1,DInteger 1])]
+
+serverDoc4 :: Document
+serverDoc4 = DMap [("number_of_hints",DInteger 10),("occupied_cols",DList [DString "labas"]),("occupied_rows",DList [DInteger 0,DInteger 2,DInteger 4,DInteger 2,DInteger 4,DInteger 1,DInteger 3,DInteger 2,DInteger 1,DInteger 1])]
+
+serverDoc5 :: Document
+serverDoc5 = DMap [("number_of_hints",DNull),("occupied_cols",DList [DInteger 2,DInteger 0,DInteger 2,DInteger 2,DInteger 2,DInteger 0,DInteger 6,DInteger 0,DInteger 3,DInteger 3]),("occupied_rows",DList [DInteger 0,DInteger 2,DInteger 4,DInteger 2,DInteger 4,DInteger 1,DInteger 3,DInteger 2,DInteger 1,DInteger 1])]
 
 hintTests :: TestTree
-hintTests = testGroup "Test hint document" []
+hintTests = testGroup "Test hint document" 
+  [   testCase "DInteger instead of DMap" $
+        hint (State {hintedCells = [Coord {col = 6, row  = 1}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6}) (DInteger 5) @?= Left "invalid DInteger 5 argument, expected DMap"
+    , testCase "DString instead of DMap" $
+        hint (State {hintedCells = [Coord {col = 6, row  = 1}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6}) (DString "labas") @?= Left "invalid DString \"labas\" argument, expected DMap"
+    , testCase "DList instead of DMap" $
+        hint (State {hintedCells = [Coord {col = 6, row  = 1}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6}) (DList [DInteger 5, DInteger 0]) @?= Left "invalid DList [DInteger 5,DInteger 0] argument, expected DMap"
+    , testCase "correct coordinates" $
+        hint (State {hintedCells = [Coord {col = 6, row  = 1}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6}) doc @?= Right (State {hintedCells = [Coord {col = 6, row = 1},Coord {col = 6, row = 2},Coord {col = 6, row = 3},Coord {col = 6, row = 4}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6})
+    , testCase "DNull instead of DMap" $
+        hint (State {hintedCells = [Coord {col = 6, row  = 1}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6}) doc2 @?= Left "invalid DNull argument, expected DInteger"
+    , testCase "DString instead of DInteger" $
+        hint (State {hintedCells = [Coord {col = 6, row  = 1}], toggledCells = [], occupiedCols = [], occupiedRows = [], noOfHints = 6}) doc3 @?= Left "invalid DString \"kazkas\" argument, expected DInteger"
+  ]
+
+doc :: Document 
+doc = DMap [("coords",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 1)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 2)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 3)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 4)]),("tail",DNull)])])])])]
+
+doc2 :: Document
+doc2 = DMap [("coords",DMap [("head",DMap [("col",DNull),("row",DNull)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 2)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 3)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 4)]),("tail",DNull)])])])])]
+
+doc3 :: Document
+doc3 = DMap [("coords",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 1)]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 2)]),("tail",DMap [("head",DMap [("col",DString "kazkas"),("row",DString "abc")]),("tail",DMap [("head",DMap [("col",DInteger 6),("row",DInteger 4)]),("tail",DNull)])])])])]
